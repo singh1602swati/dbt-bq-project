288f80eff7a9226f4df2ad81f08a9f94a939a977
Swati Singh <tech.sharmakunal@gmail.com>
2024-04-04 12:49:19 +0000

    created view model and customer table

---
+++ models/example/customer.sql
@@ -0,0 +1,12 @@
+-- customer_orders.sql
+SELECT
+  BuyerInfo_BuyerEmail AS customer_id,
+  MIN(RequestStartDate) AS customer_acquisition_date,
+  MAX(RequestStartDate) AS customer_last_order_date,
+  COUNTIF(OrderStatus = 'Shipped') AS successful_orders,
+  COUNTIF(ShipServiceLevel = 'Standard') AS standard_shipment_orders,
+  COUNTIF(ShipServiceLevel = 'Express') AS express_shipment_orders
+FROM
+  {{ ref('viewmodel') }}
+GROUP BY
+  BuyerInfo_BuyerEmail
---
+++ models/example/viewmodel.sql
@@ -0,0 +1,2 @@
+SELECT *
+FROM bigquery-dbt-project-419212.bigquery.bq_dataset
